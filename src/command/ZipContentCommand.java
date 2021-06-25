3c8b74279537f0440d6564616e1b140946d0d851
Nikola-jpg <84804348+Nikola-jpg@users.noreply.github.com>
2021-06-25 18:26:54 +0300

    First commit

---
+++ src/command/ZipContentCommand.java
@@ -0,0 +1,23 @@
+package command;
+
+import main.*;
+
+import java.util.List;
+
+public class ZipContentCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
+    }
+}
